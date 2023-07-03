8d5466b538497eeba8ab622651224c813feea6c4
Felipe <flpsnocvla@gmail.com>
2023-07-03 00:40:48 -0300

    alterando tabela usuario

--- server/sql/database/usuario.sql
+++ server/sql/database/usuario.sql
@@ -1,9 +1,10 @@
-create table if not exists `usuario` (
-	`id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`nome` varchar(255) NOT NULL,
-	`email` varchar(255) NOT NULL,
-	`senha` varchar(512) NOT NULL,
-	`ativo` boolean default true,
-	`criado` datetime,
-	`modificado` datetime
+CREATE TABLE IF NOT EXISTS `usuario` (
+	`id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`nome` VARCHAR(255) NOT NULL,
+	`email` VARCHAR(255) NOT NULL,
+	`senha` VARCHAR(512) NOT NULL,
+	`status` ENUM ('ativo', 'inativo', 'banido') DEFAULT 'ativo',
+	`tipo` ENUM ('usuario', 'admin') DEFAULT 'usuario',
+	`criado` DATETIME,
+	`modificado` DATETIME
 );
