4d1fdf3cf422c3456ce4a82c42fb4c513a2ab05b
Felipe <flpsnocvla@gmail.com>
2023-07-06 00:02:12 -0300

    Criando cadastro de usuario

--- server/controllers/usuarioController.php
+++ server/controllers/usuarioController.php
@@ -24,11 +24,67 @@ class Usuario
         ];
     }
 
-    function listar(){
+    function listar()
+    {
         $banco = new Banco();
-        
+
         return $banco->select([
             "tabela" => "usuario"
         ]);
+    }
+
+    /**
+     * Função para cadastrar um novo usuario
+     * @return array ['
+     *      "status" => boolean
+     *      "msg" => string
+     *      "code" => string //codigo da resposta
+     * ']
+     */
+    function cadastrar()
+    {
+        $banco = new Banco;
+        $funcoes = new funcoes();
+
+        $empty = $funcoes->empty([
+            "nome", "sobrenome", "email", "senha"
+        ]);
+
+        if ($empty["status"]) {
+            return [
+                "status" => false,
+                "code" => "campos",
+                "msg" => $empty["msg"]
+            ];
+        }
+
+        $usuario = $banco->select([
+            "tabela" => "usuario",
+            "igual" => [
+                "email" => $_POST["email"]
+            ]
+        ]);
+
+        if (!empty($usuario)) {
+            return [
+                "status" => false,
+                "code" => "email",
+                "msg" => "O email já está cadastrado"
+            ];
+        }
+
+        $banco->insert([
+            "nome" => $_POST["nome"] . " " . $_POST["sobrenome"],
+            "email" => $_POST["email"],
+            "senha" => hash('sha512', $_POST["senha"]),
+            "status" => "ativo",
+            "tipo" => "usuario"
+        ], "usuario");
+
+        return [
+            "status" => true,
+            "code" => "sucesso",
+            "msg" => "cadastro efetuado com sucesso"
+        ];
     }
 }
