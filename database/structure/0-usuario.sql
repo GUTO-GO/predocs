5577339a52bb06b9bf331c802c430bba1897309e
Felipe Cavalca <flpsnocvla@gmail.com>
2024-02-23 17:15:06 -0300

    Criando tabela de usuario

---
+++ database/structure/0-usuario.sql
@@ -0,0 +1,14 @@
+create table if not exists `usuario` (
+    `id` int(11) not null auto_increment,
+    `name` varchar(100) not null,
+    `email` varchar(100) not null unique,
+    `password` varchar(100) not null,
+    `created` datetime not null,
+    `modified` datetime not null,
+    primary key (`id`)
+) engine = InnoDB default charset = utf8;
+
+create index idx_usuario_email on `usuario` (`email`);
+
+insert into `usuario` (`id`, `name`, `email`, `password`, `created`, `modified`) values
+    (1, 'Administrador', 'adm@dossier.com' , '$2y$10$6Zwe9nnh/FAYR57Xn9BjPuBhvpbHIGk/NmqpqhOZdNODKneJecJ2e', now(), now());
