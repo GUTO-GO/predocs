4619cc41f0faf8b1e0bef1e281a11651bcd1cdc3
Felipe Cavalca <flpsnocvla@gmail.com>
2024-04-25 01:10:33 -0300

    Adicionando quotasize para o FTP

--- database/structure/20242504-user-ftp.sql
+++ database/structure/20242504-user-ftp.sql
@@ -3,8 +3,9 @@ CREATE TABLE users (
   Password VARCHAR(255) BINARY NOT NULL,
   Uid INT NOT NULL default '-1',
   Gid INT NOT NULL default '-1',
+  QuotaSize INT NOT NULL default '100',
   Dir VARCHAR(255) BINARY NOT NULL,
   PRIMARY KEY (User)
 );
 
-INSERT INTO users (User,Password,Uid,Gid,Dir) VALUES ('felipe',ENCRYPT('test'),'1003','1005','/storage');
+INSERT INTO users (User,Password,Uid,Gid,QuotaSize,Dir) VALUES ('felipe@email.com',ENCRYPT('test'),'1003','1005',100,'/storage');
